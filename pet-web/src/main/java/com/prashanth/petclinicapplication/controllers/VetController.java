3fecd6020289d3ed860ae8ef9f7c11820ac3b723
prashanthp <prash.7453@gmail.com>
2018-10-03 17:23:45 +0530

    Updated VetController to add VetService and constructor injecting it. Modified the vets.html page to receive the list of Vets. Closes #15.

--- pet-web/src/main/java/com/prashanth/petclinicapplication/controllers/VetController.java
+++ pet-web/src/main/java/com/prashanth/petclinicapplication/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.prashanth.petclinicapplication.controllers;
 
+import com.prashanth.petclinicapplication.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/vets", "/vets/vets.html"})
-    public String vetIndex() {
+    public String vetIndex(Model model) {
+        model.addAttribute("vetList", vetService.findAll());
         return "vets/vets";
     }
 }
