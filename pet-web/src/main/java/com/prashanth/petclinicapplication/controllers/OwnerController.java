9f0d89abe8a7b4e88280a248b0704a4028b64a57
prashanthp <prash.7453@gmail.com>
2018-10-03 17:12:06 +0530

    Updated OwnerController to add OwnerService and constructor injecting it. Modified the owners.html page to receive the list of Owners. Closes #16.

--- pet-web/src/main/java/com/prashanth/petclinicapplication/controllers/OwnerController.java
+++ pet-web/src/main/java/com/prashanth/petclinicapplication/controllers/OwnerController.java
@@ -1,13 +1,24 @@
 package com.prashanth.petclinicapplication.controllers;
 
+import com.prashanth.petclinicapplication.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    /* to get access to Owner Services and using constructor injection */
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    /* add a model to send the information to the request */
     @RequestMapping({"/owners", "/owners/owners", "/owners/owners.html"})
-    public String ownerIndex() {
+    public String ownerIndex(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/owners";
     }
 }
