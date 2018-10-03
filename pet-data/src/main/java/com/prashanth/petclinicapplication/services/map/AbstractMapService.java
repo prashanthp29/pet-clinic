e003e9cc9a252466045c3e8aa2611ee41f99dee0
prashanthp <prash.7453@gmail.com>
2018-10-03 18:22:29 +0530

    Refactored the save() method - if the object is not null, check the ID to be null if yes get the next Id value from the getNextId() method where the max id value is taken and incremented by 1(if the initial value is null then catch the Exception and assign the value to be 1L).
     Changed the OwnerServiceMap, PetServiceMap, VetServiceMap to receive only the object as parameter in save() method.
    
     deleted the setId(1L) for all objects in DataLoader.java where the id is now being generated using the getNextId() in the save() method.
    
     Closes #23.

--- pet-data/src/main/java/com/prashanth/petclinicapplication/services/map/AbstractMapService.java
+++ pet-data/src/main/java/com/prashanth/petclinicapplication/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package com.prashanth.petclinicapplication.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.prashanth.petclinicapplication.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +15,18 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        /*object.getId() cannot to resolve to type - is not identifying the object here so T extends BaseEntity
+        * and T save(ID id, T object) - id is never used we ll remove it*/
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -27,5 +36,22 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        /*max(java.util.Collections<? extends ?>) cannot be applied to (java.util.Set<ID>)
+        * So changing AbstractMapService<ID extends Long> and Map<ID> to Map<Long> */
+
+        /*if the set has null element then throw the exception and add nextId = 1
+        * else increment the nextId and hold it - to pass the value to save() while creating new Set*/
+
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        }
+        catch (NoSuchElementException nsee) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- pet-web/src/main/java/com/prashanth/petclinicapplication/bootstrap/DataLoader.java
+++ pet-web/src/main/java/com/prashanth/petclinicapplication/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.prashanth.petclinicapplication.model.Owner;
 import com.prashanth.petclinicapplication.model.Vet;
 import com.prashanth.petclinicapplication.services.OwnerService;
 import com.prashanth.petclinicapplication.services.VetService;
-import com.prashanth.petclinicapplication.services.map.OwnerServiceMap;
-import com.prashanth.petclinicapplication.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -22,31 +20,28 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        /*removing setID() from all the objects since we have captured it in the AbstractMapService to assign the values*/
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fionna");
-        owner2.setLastName("Glenane");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners list...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
